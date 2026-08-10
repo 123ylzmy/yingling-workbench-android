11e7402085255a55d595c19978bd0d64abda7c0f
123ylzmy <123ylzmy@users.noreply.github.com>
2026-08-10 13:12:57 +0000

    deploy: a2c0f3ef236a65e5b0c15e0f499c4981c1ea14c4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// 自洽日程 Service Worker v10（网络优先）
-const CACHE_NAME = 'yl-workbench-v10';
+// 自洽日程 Service Worker v11（网络优先）
+const CACHE_NAME = 'yl-workbench-v11';
 const ASSETS = [
   '/',
   '/index.html',
